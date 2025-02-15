83a44d4cdeac5e4fb7d10028d3a7f395fd19792f
JorgeGorrito <j0rg3.4b3ll4@gmail.com>
2025-02-15 07:47:47 -0500

    feat(dependencies): add utils

---
+++ anise/dependencies/utils/utils.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"reflect"
+
+	"github.com/JorgeGorrito/anise-with-gin/anise/dependencies"
+	"github.com/JorgeGorrito/anise-with-gin/anise/dependencies/errors"
+	"github.com/JorgeGorrito/anise-with-gin/anise/dependencies/types"
+)
+
+func GetAbstractType[T any]() types.Abstract {
+	var abstractType reflect.Type = reflect.TypeOf((*T)(nil)).Elem()
+	if abstractType.Kind() != reflect.Interface {
+		panic(errors.ErrAbstractMustBeInterface)
+	}
+	return abstractType
+}
+
+func Inject[T any](dependencyResolver dependencies.Resolver) T {
+	return dependencyResolver.Resolve(GetAbstractType[T]()).(T)
+}
